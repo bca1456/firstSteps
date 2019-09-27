5c4497b3d064031b468c49c497dca5ed83834a26
Yauheni <eugene.antipenko.s@gmail.com>
2019-09-27 16:30:24 +0300

    start expierence in collections

---
+++ src/collections/ExpCollections.java
@@ -0,0 +1,14 @@
+package collections;
+
+import java.util.AbstractCollection;
+import java.util.Collection;
+
+public class ExpCollections {
+    ExpCollections(){
+
+    }
+
+    public void try(){
+        Collection<Integer> collection = new Collection<Integer>();
+    }
+}
